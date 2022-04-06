01a9cb05c29b5f005ecff711ea9ff9cfb02c974e
19020442 <ltthanh304@gmail.com>
2022-04-06 17:59:52 +0700

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+const app = require("express")();
+const mysql = require("mysql");
+const PORT = 8080;
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "123",
+  database: "donations",
+});
+app.listen(PORT, () => console.log("listen on 8080"));
+
+app.get("/donations", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+
+    connection.query("SELECT * from donate", (err, rows) => {
+      connection.release();
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
+
+
+
